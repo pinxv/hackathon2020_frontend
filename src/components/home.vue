c518cc707a7352c70e254b3965b35f8d21c17243
skyswords7 <710487762@qq.com>
2020-11-28 13:45:33 +0800

    地图遮盖，导航栏

--- src/components/home.vue
+++ src/components/home.vue
@@ -73,11 +73,50 @@ export default {
   },
   mounted:function(){
 
-	 var map = new AMap.Map('map_container', {
-	        zoom:4,//级别
-	        center: [116.397428, 39.90923],//中心点坐标
-	        viewMode:'2D'//使用3D视图
-	    });
+	 var opts = {
+	     subdistrict: 0,   //返回下一级行政区
+	     extensions: 'all',  //返回行政区边界坐标组等具体信息
+	 };
+	 //实例化DistrictSearch
+	 var district = new AMap.DistrictSearch(opts);
+	 district.search('中国', function(status, result) {
+	     var bounds = result.districtList[0].boundaries;
+	     var mask = []
+	     for(var i =0;i<bounds.length;i+=1){
+	         mask.push([bounds[i]])
+	     }
+	     var disCountry = new AMap.DistrictLayer.World({
+	         zIndex:0,
+	         rejectMapMask:true
+	     })
+	     var object3Dlayer = new AMap.Object3DLayer({zIndex:1});
+	     var map = new AMap.Map('map_container', {
+	         mask:mask,
+	         center:[105,37],
+	         
+	         viewMode:'3D',
+	         labelzIndex:130,
+	         zoom:4.5,
+	         cursor:'pointer',
+	         layers:[
+	             new AMap.TileLayer({
+	                 zIndex:7
+	             }),
+	             disCountry,
+	             object3Dlayer,
+	            
+	         ]
+	     });
+	     var height = -5000;
+	     var color = '#0088ffcc';//rgba
+	     var prism = new AMap.Object3D.Wall({
+	         path:bounds,
+	         height:height,
+	         color:color
+	     });
+	     prism.transparent = true
+	     object3Dlayer.add(prism)
+	 })
   },
   //在页面加载后请求风险地区信息
   created() {
@@ -144,5 +183,10 @@ export default {
 		right: 5px;
 		bottom: 5px;
 		z-index: 2;
+	}
+	html,body,#map_container{
+	    margin:0;
+	    height:100%;
+	    background-color:rgb(216, 238,250) !important
 	}
 </style>
