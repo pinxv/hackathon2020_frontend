0c216ca812152b1ee7871a0a3bfa042e093784ee
yanhaoming <wyyanhaoming@163.com>
2020-11-28 13:46:53 +0800

    nothing

--- src/components/home.vue
+++ src/components/home.vue
@@ -2,7 +2,7 @@
   <div class="home">
 	<div class="search_place">
 	  <el-input
-	    placeholder="请输入地点"
+	    placeholder="请输入溯源码查询"
 	    prefix-icon="el-icon-search"
 	    v-model="search_place"
 	  	id="search_place">
@@ -50,7 +50,7 @@ export default {
       rotation: 0,
 	  search_place:"",
 	  //风险等级地点信息
-	  risk_level_area:[],
+	  risk_level_areas:[],
 	  list_of_risk_area_data:[{
             date: '2016-05-02',
             risk_level: '高',
@@ -71,6 +71,10 @@ export default {
     };
 	
   },
+  //在页面加载后请求风险地区信息
+  created() {
+  	this.getRiskLevelInfo();
+  },
   mounted:function(){
 
 	 var map = new AMap.Map('map_container', {
@@ -78,11 +82,21 @@ export default {
 	        center: [116.397428, 39.90923],//中心点坐标
 	        viewMode:'2D'//使用3D视图
 	    });
+	//循环构造风险地区标记
+	var i = 0;
+	for (i = 0; i < this.risk_level_areas.length; i++){
+		// 创建一个 Marker 实例：
+		var risk_level_area = this.risk_level_areas[i];
+		var marker = new AMap.Marker({
+		    position: new AMap.LngLat(risk_level_area.longitude, risk_level_area.latitude),   // 经纬度对象，也可以是经纬度构成的一维数组[116.39, 39.9]
+		    title: risk_level_area.risk_level ==='100'? "中风险":"高风险"
+		});
+		
+		// 将创建的点标记添加到已有的地图实例：
+		map.add(marker);
+	}
   },
-  //在页面加载后请求风险地区信息
-  created() {
-  	this.getRiskLevelInfo();
-  },
+  
   methods: {
 	login() {
 		this.$router.push("/login");
@@ -90,11 +104,11 @@ export default {
 	getRiskLevelInfo() {
 		var that = this;
 		// 发送请求，请求风险地区经纬度
-		this.$http.get('/risklevel/getall').then(function(response){
+		this.$http.get('RiskLevel').then(function(response){
 			if(response.data.code === 200){
 				that.$message.success("风险地区信息加载成功！");
 				//加载用户信息
-				that.risk_level_area = response.data;
+				that.risk_level_areas = response.data;
 			}
 			else {
 				that.$message.error("风险地区信息加载失败！");
